23aecadcfb3369db10ab39cab69fc3600e467eac
Junghyun <junghyun_kim3@tmax.co.kr>
2020-10-28 22:26:06 +0900

    Hello World

--- my_first_app/src/index.js
+++ my_first_app/src/index.js
@@ -1,17 +1,15 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Create React component
+const App = function () {
+    return <div> Hello, World! </div>;
+};
+
+
+// Take the React component and show it on the screen
+ReactDOM.render( 
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
