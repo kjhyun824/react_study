ba7193378c5eba5aa8cf349f606bbaffc960ee39
junghyun_kim3 <junghyun_kim3@tmax.co.kr>
2020-11-02 14:17:17 +0900

    [chore] formatting

--- my_first_app/src/index.js
+++ my_first_app/src/index.js
@@ -3,31 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function getButtonText() {
-    return 'Click Me Now!'
+  return 'Click Me Now!';
 }
 
 // Create React component
 const App = function () {
-    const button_name = 'Click Me!';
-    const button_style = { 
-        backgroundColor: 'blue', 
-        color: 'white' 
-    };
+  const button_name = 'Click Me!';
+  const button_style = {
+    backgroundColor: 'blue',
+    color: 'white',
+  };
 
-    return <div>
-        <label className="label" htmlFor="name">
-            Enter name:
-        </label>
-        <input id="name" type="text" />
-        <button style={button_style}>
-            {button_name}
-        </button>
-    </div>;
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={button_style}>{button_name}</button>
+    </div>
+  );
 };
 
-
 // Take the React component and show it on the screen
-ReactDOM.render( 
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
