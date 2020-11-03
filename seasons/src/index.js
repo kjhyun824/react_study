23a91cba3fa53a3244fe98686bf50a35a597abbd
Junghyun <woori4829@gmail.com>
2020-11-03 17:57:24 +0900

    Lecture 6 : Class Component Refactor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { latitude: null, error_message: '' };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ latitude: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ error_message: err.message });
-      },
+      (position) => this.setState({ latitude: position.coords.latitude }),
+      (err) => this.setState({ error_message: err.message }),
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.error_message && !this.state.latitude) {
       return <div>Error: {this.state.error_message}</div>;
     }
 
     if (!this.state.error_message && this.state.latitude) {
-      return <div>Latitude: {this.state.latitude}</div>;
+      return (
+        <div>
+          <SeasonDisplay latitude={this.state.latitude} />
+        </div>
+      );
     }
 
-    return <div>Loading....</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
