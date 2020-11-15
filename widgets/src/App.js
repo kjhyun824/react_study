3abdfa831a7655f2a9b11f8169429589aec00b68
junghyun_kim3 <junghyun_kim3@tmax.co.kr>
2020-11-15 16:41:57 +0900

    useEffect Practicing

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -20,6 +22,7 @@ export default () => {
   return (
     <div>
       <Accordion items={items} />
+      <Search />
     </div>
   );
 };
