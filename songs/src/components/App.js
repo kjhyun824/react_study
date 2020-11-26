3cd19919fac0da499146f8e4f086884078fc9a71
Junghyun <woori4829@gmail.com>
2020-11-26 17:49:01 +0900

    Add provider & connect

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import SongList from './SongList';
 import { selectSongs } from '../actions';
 
 const App = () => {
-  return <div>Default</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
