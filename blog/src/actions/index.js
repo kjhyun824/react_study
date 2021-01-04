5f1030985e8b0e247470dcc71ee4aa4707b17867
junghyun_kim3 <junghyun_kim3@tmax.co.kr>
2021-01-04 18:45:30 +0900

    Lecture 18 - Axios request (Wrong) -

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonPlaceholders from '../apis/jsonPlaceholders';
+
+export const fetchPosts = async () => {
+  const response = await jsonPlaceholders.get('/posts');
+
   return {
     type: 'FETCH_POST',
+    payload: response,
   };
 };
